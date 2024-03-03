52101d00c09d98082d5e117afe512811c1db3869
aljubgg <101408395+aljubgg@users.noreply.github.com>
2024-03-03 23:58:10 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,4 +2,4 @@ const fs = require('fs')
 
 global.owner = ["6283847871545"]
 global.push_kontak_delay = 5000 //5 detik, 1000 = 1 detik
-global.pairingNumber = ""
+global.pairingNumber = "6283847871545"
